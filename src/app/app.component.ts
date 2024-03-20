e9ae95725eb5d73b63e0626880c1b9aee79f92e7
reloaddev <henrik.pruess@posteo.de>
2024-03-20 11:40:39 +0100

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,54 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {NgForOf} from "@angular/common";
+import {TodoItemComponent} from "./todo-item/todo-item.component";
+import {MatButton, MatFabButton} from "@angular/material/button";
+import {MatIcon} from "@angular/material/icon";
+import {MatToolbar} from "@angular/material/toolbar";
+import {CdkDragDrop, CdkDropList, moveItemInArray} from "@angular/cdk/drag-drop";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, NgForOf, TodoItemComponent, MatButton, MatFabButton, MatIcon, MatToolbar, CdkDropList, FormsModule],
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'todo-list';
+export class AppComponent implements OnInit {
+
+  todos: string[] = [];
+
+  ngOnInit() {
+    const storedTodos = localStorage.getItem('storedTodos');
+    this.todos = JSON.parse(storedTodos === null ? '' : storedTodos);
+  }
+
+  addNewItem() {
+    this.todos.push("");
+    this.saveTodoItems();
+  }
+
+  updateTodo(event: any, index: number) {
+    this.todos[index] = event;
+    this.saveTodoItems();
+  }
+
+  removeItem(i: number) {
+    this.todos.splice(i, 1);
+    this.saveTodoItems();
+  }
+
+  dropItem(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.todos, event.previousIndex, event.currentIndex);
+    this.saveTodoItems();
+  }
+
+  private saveTodoItems() {
+    localStorage.setItem('storedTodos', JSON.stringify(this.todos));
+  }
+
+  trackByIndex(index: number, obj: any): any {
+    return index;
+  }
+
 }
---
+++ src/app/todo-item/todo-item.component.ts
@@ -0,0 +1,34 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {NgForOf} from "@angular/common";
+import {MatCheckbox} from "@angular/material/checkbox";
+import {CdkDrag} from "@angular/cdk/drag-drop";
+import {FormsModule} from "@angular/forms";
+
+@Component({
+  selector: 'todo-item',
+  standalone: true,
+  imports: [
+    NgForOf,
+    MatCheckbox,
+    CdkDrag,
+    FormsModule
+  ],
+  templateUrl: './todo-item.component.html'
+})
+export class TodoItemComponent {
+
+  @Input() todo!: string;
+  @Output() todoChange = new EventEmitter<string>();
+  @Output() deleteTodoEvent = new EventEmitter<void>();
+
+  removeTodo() {
+    setTimeout(() => {
+      this.deleteTodoEvent.emit();
+    }, 1000)
+  }
+
+  onTodoChange() {
+    this.todoChange.emit(this.todo);
+  }
+
+}
